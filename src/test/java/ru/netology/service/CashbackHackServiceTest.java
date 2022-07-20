435cc5d7461bf70d9a0e5c7b5fe138941a036ce1
Анна Никулина <anna.nikulina89@mail.ru>
2022-07-20 21:22:06 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,55 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
-    
+    @Test
+    public void BonusServiceNull() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(0);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void BonusServiceLimitValueMin() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 50;
+        int actual = service.remain(950);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void BonusServiceLimitValueMax() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void BonusServiceLimitValue() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void BonusServiceBigValue() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 571;
+        int actual = service.remain(1736879429);
+
+        assertEquals(actual, expected);
+    }
+
+
 }
